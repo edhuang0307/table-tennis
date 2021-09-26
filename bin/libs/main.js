6eca77404f58134536da971952b94fee9aae34a1
edhuang0307 <edhuang0307@gmail.com>
2021-09-27 06:58:58 +0800

    在 ts 文件獲取 html 中的元素

--- bin/libs/main.js
+++ bin/libs/main.js
@@ -1,3 +1,17 @@
 "use strict";
-console.log("hello world");
+const table = document.getElementById("table");
+const net = document.getElementById("net");
+const ball = document.getElementById("ball");
+const player01 = {
+    paddle: document.getElementById("paddle-01"),
+    score: document.getElementById("score-01"),
+};
+const player02 = {
+    paddle: document.getElementById("paddle-02"),
+    score: document.getElementById("score-02"),
+};
+table.addEventListener("mousemove", (event) => {
+    event.movementX, event.movementY;
+    console.log(event.movementX, event.movementY);
+});
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+const table = document.getElementById("table") as HTMLDivElement;
+const net = document.getElementById("net") as HTMLDivElement;
+const ball = document.getElementById("ball") as HTMLDivElement;
+
+
+const player01 = {
+    paddle: document.getElementById("paddle-01") as HTMLDivElement,
+    score: document.getElementById("score-01") as HTMLDivElement,
+};
+
+const player02 = {
+    paddle: document.getElementById("paddle-02") as HTMLDivElement,
+    score: document.getElementById("score-02") as HTMLDivElement,
+};
+
+table.addEventListener("mousemove", (event: MouseEvent) => {
+    event.movementX, event.movementY
+    console.log(event.movementX, event.movementY)
+})
