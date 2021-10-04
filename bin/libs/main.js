ed3f19d62163c25ca8e5e541323579d7ab4ecfbf
Puzz Chang <oggpbo.feprogvi1@w-techsolutions.com>
2021-10-04 22:40:03 +0800

    滑鼠事件與元素的座標參數；元素跟隨滑鼠功能；方法建議

--- bin/libs/main.js
+++ bin/libs/main.js
@@ -2,16 +2,46 @@
 const table = document.getElementById("table");
 const net = document.getElementById("net");
 const ball = document.getElementById("ball");
-const player01 = {
+const player1 = {
     paddle: document.getElementById("paddle-01"),
     score: document.getElementById("score-01"),
 };
-const player02 = {
+const player2 = {
     paddle: document.getElementById("paddle-02"),
     score: document.getElementById("score-02"),
 };
+//TODO 研究此 3 個坐標系參數的差異
+// console.log(player2.paddle.offsetTop, player2.paddle.offsetLeft);
+// console.log(player2.paddle.clientTop, player2.paddle.clientTop);
+// console.log(player2.paddle.scrollTop, player2.paddle.scrollLeft);
 table.addEventListener("mousemove", (event) => {
-    event.movementX, event.movementY;
-    console.log(event.movementX, event.movementY);
+    //TODO 研究此 6 個坐標系參數的差異
+    // console.log(event.offsetX, event.offsetY)
+    // console.log(event.clientX, event.clientY)
+    // console.log(event.movementX, event.movementY)
+    // console.log(event.screenX, event.screenY)
+    // console.log(event.pageX, event.pageY)
+    // console.log(event.x, event.y)
+    //TODO 處理 table.style.border: 去單位 >> 字串轉數字 >> * 2
+    const border = 20;
+    const half = player1.paddle.offsetHeight / 2;
+    let y = event.offsetY;
+    if (y < half) {
+        y = half;
+    }
+    else if (y >= table.offsetHeight - half - border) {
+        y = table.offsetHeight - half - border;
+    }
+    //TODO clamp()
+    player1.paddle.style.top = `${y - player1.paddle.offsetHeight / 2}px`;
 });
+function clamp(value, max, min) {
+    //TODO 當 value 大於 max，回傳 max；當小於 min，回傳 min；否則依舊是 value
+    return value;
+}
+function setScore(element, scroe) {
+    //TODO 當 score 是 number 型別: 直接賦值；若為 +，表示 +1；若為 -，表示 -1
+}
+//TODO 讓電腦可以自動上下移動 setInterval + player1.paddle.style.top
+//TODO 思考 AI 的移動方式，並寫於 .md 文件中
 //# sourceMappingURL=main.js.map
