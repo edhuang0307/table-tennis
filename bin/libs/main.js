367744d63a4ef476f86d3996ae6680c34232f3e3
edhuang0307 <edhuang0307@gmail.com>
2021-10-18 06:29:19 +0800

    1. 調整球桌位置 2. 添加部份作業代碼

--- bin/libs/main.js
+++ bin/libs/main.js
@@ -2,16 +2,45 @@
 const table = document.getElementById("table");
 const net = document.getElementById("net");
 const ball = document.getElementById("ball");
-const player01 = {
+const player1 = {
     paddle: document.getElementById("paddle-01"),
     score: document.getElementById("score-01"),
 };
-const player02 = {
+const player2 = {
     paddle: document.getElementById("paddle-02"),
     score: document.getElementById("score-02"),
 };
+// const half = player1.paddle.offsetHeight / 2
+// function clamp(value: number, max: number, min: number) {
+//     //todo 當 Value 大於 max，回傳 max; 當小於 min，回傳 mins，否則依舊是 value
+//     let y = event.offsetY as number;
+//     if (y < half) {
+//         y = half ;
+//     }
+//     else if (y >= table.offsetHeight - half - vwToPx * 2) {
+//         y = table.offsetHeight - half - vwToPx * 2;
+//     }
+// }
 table.addEventListener("mousemove", (event) => {
-    event.movementX, event.movementY;
-    console.log(event.movementX, event.movementY);
+    //todo 處理 table. style.border, 去單位 >> 字串轉數字 * 2
+    table.style.border = "13px solid lightslategray";
+    const border = table.style.border;
+    let result1 = "";
+    for (let i = 0; i < border.length; i++) {
+        if (border[i].match(/[0-9.]/)) {
+            result1 += border[i];
+        }
+    }
+    const strToNum = +result1;
+    const half = player1.paddle.offsetHeight / 2;
+    let y = event.offsetY;
+    if (y < half) {
+        y = half;
+    }
+    else if (y >= table.offsetHeight - half - strToNum * 2) {
+        y = table.offsetHeight - half - strToNum * 2;
+    }
+    // const y = event.offsetY - player1.paddle.offsetHeight / 2;
+    player1.paddle.style.top = `${y - half}px`;
 });
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,65 @@ const table = document.getElementById("table") as HTMLDivElement;
 const net = document.getElementById("net") as HTMLDivElement;
 const ball = document.getElementById("ball") as HTMLDivElement;
 
-
-const player01 = {
+const player1 = {
     paddle: document.getElementById("paddle-01") as HTMLDivElement,
     score: document.getElementById("score-01") as HTMLDivElement,
 };
 
-const player02 = {
+const player2 = {
     paddle: document.getElementById("paddle-02") as HTMLDivElement,
-    score: document.getElementById("score-02") as HTMLDivElement,
+    score: document.getElementById("score-02") as HTMLDivElement, 
 };
 
+
+
+
+// const half = player1.paddle.offsetHeight / 2
+
+// function clamp(value: number, max: number, min: number) {
+//     //todo 當 Value 大於 max，回傳 max; 當小於 min，回傳 mins，否則依舊是 value
+//     let y = event.offsetY as number;
+    
+//     if (y < half) {
+//         y = half ;
+//     }
+//     else if (y >= table.offsetHeight - half - vwToPx * 2) {
+//         y = table.offsetHeight - half - vwToPx * 2;
+//     }
+// }
+
+
+
+
+
 table.addEventListener("mousemove", (event: MouseEvent) => {
-    event.movementX, event.movementY
-    console.log(event.movementX, event.movementY)
-})
+    //todo 處理 table. style.border, 去單位 >> 字串轉數字 * 2
+    table.style.border = "13px solid lightslategray";
+    const border = table.style.border;
+
+    let result1 = "" as string;
+    for (let i = 0; i < border.length; i++) {
+        if (border[i].match(/[0-9.]/)) {
+            result1 += border[i];
+        }
+    }
+    const strToNum = +result1;
+    const half = player1.paddle.offsetHeight / 2
+
+    let y = event.offsetY;
+    if (y < half) {
+        y = half ;
+    }
+    else if (y >= table.offsetHeight - half - strToNum * 2) {
+        y = table.offsetHeight - half - strToNum * 2;
+    }
+    // const y = event.offsetY - player1.paddle.offsetHeight / 2;
+    player1.paddle.style.top = `${ y - half }px`;
+})  
+
+
+
+
+
+ 
+
