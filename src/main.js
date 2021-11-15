a4001d23775d4c78b586324e275f277ccf675b14
edhuang0307 <edhuang0307@gmail.com>
2021-11-15 09:37:31 +0800

    添加移動求代碼，以及一些未完成功能的代碼

--- src/main.js
+++ src/main.js
@@ -1,2 +1,84 @@
 "use strict";
-console.log("hello world");
+var fps = 1000 / 30;
+var table = document.getElementById("table");
+var net = document.getElementById("net");
+var ball = {
+    ball: document.getElementById("ball"),
+    data: {
+        rate: 6,
+        point: [0, 0],
+        direction: [1, 1],
+    }
+};
+var player1 = {
+    paddle: document.getElementById("paddle1"),
+    score: document.getElementById("score1"),
+};
+var player2 = {
+    paddle: document.getElementById("paddle2"),
+    score: document.getElementById("score2"),
+    data: {
+        /** 每幀的移動離 (像素)*/
+        rate: 2,
+        y: 0,
+        direction: 1,
+    },
+};
+var tool = {
+    border: getComputedStyle(table).borderWidth,
+    width: +(this.border.match(/\d+/)[0]),
+    getBorder: function () {
+        var border = this.border;
+    },
+    getWidth: function () {
+        var width = this.width;
+    },
+};
+// note getComputedStyle() 方法獲取指定元素的所有實際（計算）CSS 屬性和值。
+// 處理玩家的控制
+table.addEventListener("mousemove", function (event) {
+    // const border = getComputedStyle(table).borderWidth; // 先取出 13px
+    // const width = +border.match(/\d+/)![0]; // ! 表示一定找得到資料; 再取出 13，轉成 number
+    // const width = +(border.match(/\d+/)?.[0] || 0);  
+    tool.getBorder();
+    tool.getWidth();
+    var half = player1.paddle.offsetHeight / 2; // 球拍高度 / 2
+    var y = event.offsetY;
+    if (y < half) {
+        y = half;
+    }
+    else if (y >= table.offsetHeight - half - width * 2) {
+        y = table.offsetHeight - half - width * 2;
+    }
+    player1.paddle.style.top = y - half + "px";
+});
+window.addEventListener("load", function (event) {
+    // 移動電腦
+    var border = getComputedStyle(table).borderWidth;
+    var width = +border.match(/\d+/)[0]; // ! 表示一定找得到資料
+    setInterval(function () {
+        if (player2.data.y >= table.offsetHeight - player1.paddle.offsetHeight - width * 2)
+            player2.data.direction = -1;
+        else if (player2.data.y <= 0)
+            player2.data.direction = 1;
+        player2.data.y += player2.data.rate * player2.data.direction;
+        player2.paddle.style.top = player2.data.y + "px";
+    }, fps);
+    // 移動球
+    setInterval(function () {
+        // 控制球 y 軸方向
+        if (ball.data.point[1] >= table.offsetHeight - ball.ball.offsetHeight - 25)
+            ball.data.direction[1] = -1;
+        else if (ball.data.point[1] <= 0)
+            ball.data.direction[1] = 1;
+        ball.data.point[1] += ball.data.rate * ball.data.direction[1];
+        ball.ball.style.top = ball.data.point[1] + "px";
+        // 控制球 x 軸方向
+        if (ball.data.point[0] >= table.offsetWidth - ball.ball.offsetWidth - 25)
+            ball.data.direction[0] = -1;
+        else if (ball.data.point[0] <= 0)
+            ball.data.direction[0] = 1;
+        ball.data.point[0] += ball.data.rate * ball.data.direction[0];
+        ball.ball.style.left = ball.data.point[0] + "px";
+    }, fps);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
+type Direction = -1 | 0 | 1;
+const fps = 1000 / 30;
+
 const table = document.getElementById("table") as HTMLDivElement;
 const net = document.getElementById("net") as HTMLDivElement;
-const ball = document.getElementById("ball") as HTMLDivElement;
+const ball = {
+    ball: document.getElementById("ball") as HTMLDivElement,
+    data: {
+        rate: 6,
+        x: 0,
+        y: 0,
+        directionX: 1,
+        directionY: 1, 
+    }
+};
 
 const player1 = {
     paddle: document.getElementById("paddle1") as HTMLDivElement,
@@ -8,70 +20,120 @@ const player1 = {
 };
 
 const player2 = {
-    paddle: document.getElementById("paddle2") as HTMLDivElement,
+    paddle: document.getElementById("paddle2") as HTMLDivElement, 
     score: document.getElementById("score2") as HTMLDivElement, 
+    data: {
+        /** 每幀的移動離 (像素)*/ 
+        rate: 2,
+        y: 0,
+        direction: 1 as -1 | 0 | 1,
+    },
 };
 
-
-
-
-// const half = player1.paddle.offsetHeight / 2
-
-// function clamp(value: number, max: number, min: number) {
-//     //todo 當 Value 大於 max，回傳 max; 當小於 min，回傳 mins，否則依舊是 value
-//     let y = event.offsetY as number;
-    
-//     if (y < half) {
-//         y = half ;
-//     }
-//     else if (y >= table.offsetHeight - half - vwToPx * 2) {
-//         y = table.offsetHeight - half - vwToPx * 2;
-//     }
+// let tool = {
+//     border: getComputedStyle(table).borderWidth,
+//     width: +(this.border.match(/\d+/)![0]),
+//     getBorder: function () {
+//         const border = this.border;
+//     },
+//     getWidth: function () {
+//         const width = this.width;
+//     },
 // }
 
+// 玩家跟電腦有同樣常數，宣告方法
+function borderAndWidth(t:HTMLDivElement) {
+    const border = getComputedStyle(t).borderWidth;
+    const width = +(border.match(/\d+/)?.[0] || 0);  
+}
 
 
+// note getComputedStyle() 方法獲取指定元素的所有實際（計算）CSS 屬性和值。
+// 處理玩家的控制
+table.addEventListener("mousemove", (event) => {
 
+    // borderAndWidth(table);
 
-table.addEventListener("mousemove", (event: MouseEvent) => {
-    //todo 處理 table. style.border, 去單位 >> 字串轉數字 * 2
-    table.style.border = "13px solid lightslategray";
-    const border = table.style.border;
+    const border = getComputedStyle(table).borderWidth; // 先取出 13px
+    const width = +(border.match(/\d+/)?.[0] || 0);  
 
-    let result1 = "" as string;
-    for (let i = 0; i < border.length; i++) {
-        if (border[i].match(/[0-9.]/)) {
-            result1 += border[i];
-        }
-    }
-    const strToNum = +result1;
-    const half = player1.paddle.offsetHeight / 2
+    const half = player1.paddle.offsetHeight / 2 // 球拍高度 / 2
 
     let y = event.offsetY;
     if (y < half) {
-        y = half ;
+        y = half;
     }
-    else if (y >= table.offsetHeight - half - strToNum * 2) {
-        y = table.offsetHeight - half - strToNum * 2;
+    else if (y >= table.offsetHeight - half - width * 2) {
+        y = table.offsetHeight - half - width * 2;
     }
-    // const y = event.offsetY - player1.paddle.offsetHeight / 2;
     player1.paddle.style.top = `${ y - half }px`;
 })  
 
-
-var y = 0;
-window.onload = function() {
-    setInterval(function() 
-    {   
-        player2.paddle.style.top = y;
-        if (y < +(table.offsetHeight)) {
-            y++;
-        }
-        else if (y > +(table.offsetHeight) - player2.paddle.offsetHeight) {
-            y--;
-        }
-    }, 10);
+// 得分後重新發球
+function reset() {
+    ball.data.x = +(table.style.width) / 2;
+    ball.data.y = +(table.style.height) / 2;
+    ball.data.directionX = -ball.data.directionX;
+    ball.data.rate = 6;
 }
 
+
+addEventListener("load", event => {
+    // 移動電腦
+    const border = getComputedStyle(table).borderWidth;
+    const width = +border.match(/\d+/)![0]; // ! 表示一定找得到資料
+
+    setInterval(() => {
+        if (player2.data.y >= table.offsetHeight - player1.paddle.offsetHeight - width * 2) 
+            player2.data.direction = -1;
+        
+        else if (player2.data.y <= 0)
+            player2.data.direction = 1;
+
+        player2.data.y += player2.data.rate * player2.data.direction;
+        player2.paddle.style.top = `${ player2.data.y }px`;
+    }, fps)
+
+
+    // 移動球
+    setInterval(() => {
+
+        // 控制球 y 軸方向
+        if (ball.data.y >= table.clientHeight - 25) 
+        ball.data.directionY = -1;
+        
+        else if (ball.data.y <= 0)
+            ball.data.directionY = 1;
+
+        ball.data.y +=ball.data.rate * ball.data.directionY;
+        ball.ball.style.top = `${ ball.data.y }px`;
+
+        // 控制球 x 軸方向
+        if (ball.data.x >= table.offsetWidth - ball.ball.offsetWidth -25 ) {
+            reset() // 重新開球
+            ball.data.directionX = -1;
+        }
+        
+    
+        else if (ball.data.x <= 0) {
+            reset() // 重新開球
+            ball.data.directionX = 1;
+        }
+
+        ball.data.x +=ball.data.rate * ball.data.directionX;
+        ball.ball.style.left = `${ ball.data.x }px`;
+
+    }, fps)
+
+})
+
+
+// 更新分數
+function updateScore() {
+    if (ball.data.x - +(ball.ball.style.width) < 0) {
+        // player1.score.innerText++
+        reset();
+    }
+}
 
 
